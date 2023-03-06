02ddaf1913f9b6c7c9db15c138fb9dfb3a67141f
AbellasJuan <juanvictoraa@gmail.com>
2023-03-06 19:47:31 -0300

    feat: create single food page

--- frontend/src/app/services/cart.service.ts
+++ frontend/src/app/services/cart.service.ts
@@ -9,7 +9,7 @@ import { Food } from '../shared/models/Food';
 })
 export class CartService {
 
-  private cart: Cart = new Cart();
+  private cart: Cart = this.getCartFromLocalStorage();
   private cartSubject: BehaviorSubject<Cart> = new BehaviorSubject(this.cart);
 
   constructor() { }
@@ -20,10 +20,12 @@ export class CartService {
     return;
 
     this.cart.items.push(new CartItem(food));
+    this.setCartToLocalStorage();
   }
 
   removeFromCart(foodId: string):void{
-    this.cart.items = this.cart.items.filter(item => item.food.id != foodId)
+    this.cart.items = this.cart.items.filter(item => item.food.id != foodId);
+    this.setCartToLocalStorage();
   }
 
   changeQuantity(foodId: string, quantity: number){
@@ -33,16 +35,31 @@ export class CartService {
 
     cartItem.quantity = quantity;
     cartItem.price = quantity*cartItem.food.price;
+    this.setCartToLocalStorage();
   }
 
   clearCart(){
     this.cart = new Cart();
+    this.setCartToLocalStorage();
   }
 
   getCartObservable():Observable<Cart>{
     return this.cartSubject.asObservable();
   }
 
+  private setCartToLocalStorage():void{
+    this.cart.totalPrice = this.cart.items.reduce((prevSum, currentItem) => prevSum + currentItem.price, 0);
 
+    this.cart.totalCount = this.cart.items.reduce((prevSum, currentItem) => prevSum + currentItem.quantity, 0)
+
+    const cartJson = JSON.stringify(this.cart);
+    localStorage.setItem('Cart', cartJson);
+
+    this.cartSubject.next(this.cart);
+  }
+
+  private getCartFromLocalStorage():Cart{
+    const cartJson = localStorage.getItem('Cart');
+    return cartJson? JSON.parse(cartJson) : new Cart();
+  }
 }
-
